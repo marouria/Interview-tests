c53991a2ddc50d8dae919538bda86a660c905bf9
Marie Leuliette <marie.leuliette@gmail.com>
2021-07-27 18:27:12 +0200

    without permutation method

--- Exercice1.rb
+++ Exercice1.rb
@@ -1,4 +1,4 @@
-def encryption(string)
+def encryption_with_permutation(string)
   array = string.chars.permutation(1).to_a.map.with_index do |character, index|
     if character.first.match?(/[[:alpha:]]/) && index.odd?
       character.first.succ.downcase
@@ -9,9 +9,24 @@ def encryption(string)
     else character.first
     end
   end
+  array.reverse.join
+end
 
+def encryption_without_permutation(string)
+  array = string.chars.map.with_index do |character, index|
+    if character.match?(/[[:alpha:]]/) && index.odd?
+      character.succ.downcase
+    elsif character.match?(/[[:alpha:]]/) && index.even?
+      character.succ
+    elsif character.match?(/[[:digit:]]/)
+      9 - character.to_i
+    else character
+    end
+  end
   array.reverse.join
 end
 
 p "$$$$$$$$$$$$$$$$$"
-p encryption("BORN IN 2015!")
+p encryption_with_permutation("BORN IN 2015!")
+p "$$$$$$$$$$$$$$$$$"
+p encryption_without_permutation("BORN IN 2015!")
